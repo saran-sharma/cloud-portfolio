a9982da3bd5f5bce4a485509dfe0137cfea09d78
Saran S (Capgemini America Inc) <v-sarans2@microsoft.com>
2026-07-11 13:10:31 +0530

    Added profile pic

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,4 +1,4 @@
-import profileImg from "@/assets/profilepic.png";
+import profileImg from "@/assets/profilepic.jpeg";
 import p1 from "@/assets/project-1.jpg";
 import p2 from "@/assets/project-2.jpg";
 import p3 from "@/assets/project-3.jpg";
